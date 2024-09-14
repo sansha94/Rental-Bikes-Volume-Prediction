63c1b362ec79221b7aba9a69101b861bd188e32c
sank3t <sanketsharma2196@gmail.com>
2024-09-14 23:23:37 +0530

    Added function to show boxplot and made figsize as optional argument

--- utils/plotting.py
+++ utils/plotting.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 
 def show_hist(
     df: pd.DataFrame,
-    x_axis: str
+    x_axis: str,
+    fig_size=(8, 5)
 ):
-    plt.figure(figsize=(8, 5))
+    plt.figure(figsize=fig_size)
     sns.histplot(x=x_axis, data=df)
     plt.show()
 
 
 def show_heatmap(
     df: pd.DataFrame,
-    nmrcl_cols: list
+    nmrcl_cols: list,
+    fig_size=(10, 6)
 ):
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=fig_size)
     sns.heatmap(df[nmrcl_cols].corr(), annot=True)
     plt.show()
 
@@ -24,10 +26,26 @@ def show_heatmap(
 def show_time_series(
     df: pd.DataFrame,
     x_axis: str,
-    y_axis: str
+    y_axis: str,
+    fig_size=(10, 6)
 ):
-    plt.figure(figsize=(10, 6))
+    plt.figure(figsize=fig_size)
     sns.lineplot(
+        x=x_axis,
+        y=y_axis,
+        data=df
+    )
+    plt.show()
+
+
+def show_boxplot(
+    df: pd.DataFrame,
+    x_axis: str,
+    y_axis: str,
+    fig_size=(10, 6)
+):
+    plt.figure(figsize=fig_size)
+    sns.boxplot(
         x=x_axis,
         y=y_axis,
         data=df
